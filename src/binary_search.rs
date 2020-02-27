de699685495cf96b9489017effd5328a2326cbeb
Sean Hellum <seanhellum45@gmail.com>
2020-02-27 21:14:32 +0000

    update

--- src/binary_search.rs
+++ src/binary_search.rs
@@ -1,4 +1,16 @@
 use std::marker::Sized;
-pub fn binary_search<T: Sized>(array: &[T]) -> T {
-    return *array.get(0).unwrap()
+pub fn binary_search<T: Sized + PartialEq + PartialOrd>(array: &[T], target: T) -> bool {
+    let mut low = 0;
+    let mut high = array.len()-1;
+    while low <= high {
+        let mid = (low + (high - low)/2) as usize;
+        if array[mid] == target {
+            return true;
+        } else if *array.get(mid).unwrap() < target {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+    return false;
 }

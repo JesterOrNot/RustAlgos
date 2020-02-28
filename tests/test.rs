38b6160fbd36ac1fc2c95d90766a3c0caf62abdf
JesterOrNot <seanhellum45@gmail.com>
2020-02-27 20:07:19 -0600

    Add another unit test

--- tests/test.rs
+++ tests/test.rs
@@ -1,4 +1,5 @@
 use rust_algo::binary_search;
+
 #[test]
 pub fn test_binary_search_1() {
     let my_arr: [i32; 3] = [1, 2, 3];
@@ -16,3 +17,10 @@ pub fn test_binary_search_3() {
     let my_arr: [i32; 6] = [1, 2, 3, 5, 22, 100];
     assert_eq!(binary_search(&my_arr, -2), false)
 }
+
+#[test]
+pub fn test_binary_search_4() {
+    let my_arr: [i32; 7] = [-66, 1, 2, 3, 5, 22, 100];
+    assert_eq!(binary_search(&my_arr, -2000), false)
+}
+
